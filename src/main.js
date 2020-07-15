4f9e59ee1b242abb1f1471248d50332eb006f850
CodingWithAlice <48755515+CodingWithAlice@users.noreply.github.com>
2020-07-15 17:34:41 +0800

    初始提交

---
+++ src/main.js
@@ -0,0 +1,109 @@
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(childComp),
+// }).$mount('#app')
+
+// let childComp = { 
+//     template: '<div>{{msg}}</div>', 
+//     created() { 
+//         console.log('child created') 
+//     },
+//     mounted() { 
+//         console.log('child mounted') 
+//     },
+//     data() { 
+//         return { 
+//             msg: 'Hello Vue' 
+//         } 
+//     } 
+// }
+
+// Vue.mixin({ 
+//     created() { 
+//         console.log('parent created') 
+//     } 
+// })
+// new Vue({ 
+//     el: '#app', 
+//     render: h => h(childComp) 
+// })
+// 全局注册代码
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.component('app', App)
+
+// new Vue({
+//     el: '#app',
+//     template: '<app></app>'
+// })
+// 异步组件--工厂函数
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.component('HelloWorld', function (resolve) {
+//     // 这个特殊的 require 语法将告诉 webpack 自动将编译后的代码分割成不同的块
+//     require(['./components/HelloWorld'], function(res) {
+//         resolve(res);
+//     })
+// })
+
+// new Vue({
+//     el: '#app',
+//     render(h) {
+//         return h(App)
+//     }
+// })
+// 异步组件--Promise
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.component('HelloWorld', 
+//     // 该'import'函数返回一个'Promise'对象
+//     () => import('./components/HelloWorld.vue')
+// )
+
+// new Vue({
+//     el: '#app',
+//     render(h) {
+//         return h(App)
+//     }
+// })
+// 异步组件--高级异步组件
+import Vue from 'vue'
+import App from './App.vue'
+// import { component } from 'vue/types/umd'
+
+const LoadingComp = {
+    template: '<div>loading</div>'
+}
+
+const ErrorComp = {
+    template: '<div>error</div>'
+}
+
+const AsyncComp = () => ({
+    // 需要加载的组件，应当是一个Promise【借助webpack的import语法】
+    component: import('./components/HelloWorld.vue'),
+    // 加载中应当渲染的组件
+    loading: LoadingComp,
+    // 出错时渲染的组件
+    error: ErrorComp,
+    // 渲染加载中组件前的等待时间，默认200ms
+    delay: 200,
+    // 最长等待时间，超出此时间则渲染错误组件，默认 infinity
+    timeout: 1000
+})
+
+Vue.component('HelloWorld', AsyncComp)
+
+new Vue({
+    el: '#app',
+    render(h) {
+        return h(App)
+    }
+})
