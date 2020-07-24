3a9156887cf0a833f7b0c0b1292ed34d19251692
CodingWithAlice <48755515+CodingWithAlice@users.noreply.github.com>
2020-07-24 15:21:24 +0800

    这是响应式对象调试案例

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld/>
-  </div>
+  </div> -->
+    <!-- 这是响应式对象的实例调试 -->
+    <div>
+        <h1> {{ nested.msg }} </h1>
+        <ul>
+            <li>
+                <a href="#">这是一个链接</a>
+            </li>
+            <li>
+                <a href="#">这是二个链接</a>
+            </li>
+            <li>
+                <a href="#">这是三个链接</a>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -12,7 +27,14 @@ export default {
   name: 'app',
   components: {
     // HelloWorld
-  }
+  },
+  data() {
+      return {
+          nested: {
+              msg: '这是响应式对象的实例调试'
+          }
+      }
+  },
 }
 </script>
 
