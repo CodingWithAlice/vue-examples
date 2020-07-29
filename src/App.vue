44535b23b23ea591cdca01f9d9c4464811398d27
CodingWithAlice <48755515+CodingWithAlice@users.noreply.github.com>
2020-07-29 14:31:30 +0800

    nextTick的实例

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
         </ul>
     </div> -->
     <!-- 这是依赖收集的实例调试 -->
-    <div id="app">
+    <!-- <div id="app">
         <img src="./assets/logo.png">
         <div v-if="flag">
             {{ msg }}
@@ -29,38 +29,63 @@
         </div>
         <button @click="change">change</button>
         <button @click="toggle">toggle</button>
+    </div> -->
+    <!-- nextTick -->
+    <div id="app">
+        <img src="./assets/logo.png">
+        <div ref="msg">
+            {{ msg }}
+        </div>
+        <button @click="change">change</button>
     </div>
 </template>
+
+
+
+
+
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {// HelloWorld
-},
+  data() {return {msg: '这是初始msg',}},
+  methods: {
+      change() {
+          this.$nextTick(() => {
+              console.log('nextTick:', this.$refs.msg.innerText);
+          });
+          this.msg = '这是change方法中的msg改变';
+          console.log('sync:', this.$refs.msg.innerText);         
+          this.$nextTick().then(() => {
+              console.log('nextTick with Promise:', this.$refs.msg.innerText);
+          });
+      }
+  }
+//   components: {// HelloWorld},
 //   这是依赖收集的实例
-    data() {
-        return {
-            flag: true,
-            msg: '这是msg',
-            msg1: '这是msg1',
-        }
-    },
-    methods: {
-        change() {
-            this.msg = Math.random();
-        },
-        toggle() {
-            this.flag = !this.flag;
-        }
-    },
+    // data() {
+    //     return {
+    //         flag: true,
+    //         msg: '这是msg',
+    //         msg1: '这是msg1',
+    //     }
+    // },
+    // methods: {
+    //     change() {
+    //         this.msg = Math.random();
+    //     },
+    //     toggle() {
+    //         this.flag = !this.flag;
+    //     }
+    // },
     // 写一个会触发无限更新的bug，自定义一个watcher
-    watch: {
-        msg() {
-            this.msg = Math.random();
-        },
-    },
+    // watch: {
+    //     msg() {
+    //         this.msg = Math.random();
+    //     },
+    // },
 //   这是响应式对象的实例调试
 //   data() {
 //       return {
